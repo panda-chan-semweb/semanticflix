1befd9e6af25ac603536faa9b5ba064c1675d9e9
annidasa <annida.safira@gmail.com>
2020-12-17 20:30:28 +0700

    init

--- lists/utils.py
+++ lists/utils.py
@@ -85,7 +85,7 @@ def executeQueryJSONLDDBPedia(query):
 
 def generateSearchQuery(keyword, release_year = None, rating = None, show_type = None):
     query = prefixes + '''
-        SELECT ?type_label ?title (GROUP_CONCAT(?cast;SEPARATOR=", ") AS ?casts) ?director ?description (str(?release_year) as ?release_year_value) ?rating_label
+        SELECT ?type_label ?title (GROUP_CONCAT(?cast;SEPARATOR=", ") AS ?casts) (GROUP_CONCAT(?director;SEPARATOR=", ") AS ?directors) ?description (str(?release_year) as ?release_year_value) ?rating_label
         WHERE {
             ?netflixShow rdf:type ?type ;
                         netflix:title ?title ;
@@ -116,7 +116,7 @@ def generateSearchQuery(keyword, release_year = None, rating = None, show_type =
     query = query + '''
             FILTER (regex(?title, "%s", "i") || regex(?cast, "%s", "i") || regex(?director, "%s", "i") || regex(?description, "%s", "i"))
         }
-        GROUP BY ?type_label ?title ?director ?description ?release_year ?rating_label
+        GROUP BY ?type_label ?title ?description ?release_year ?rating_label
     ''' % (keyword, keyword, keyword, keyword)
 
     return query
